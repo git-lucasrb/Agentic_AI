82feec6852d02c92a0a7f95e975e3c48f8a63bab
Lucas Ribeiro <lucas.rb.solutions@gmail.com>
2025-06-26 18:17:29 -0300

    Streamlit working

---
+++ src/langgraphagenticai/ui/streamlitui/ui_configfile.py
@@ -0,0 +1,25 @@
+from configparser import ConfigParser
+import os
+
+class Config:
+   #def __init__(self,config_file="/src/langgraphagenticai/ui/uiconfigfile.ini"):
+    def __init__(self, config_file=None):
+        if config_file is None:
+            config_file = os.path.join(os.path.dirname(__file__), 'uiconfigfile.ini')
+
+        self.config=ConfigParser()
+        self.config.read(config_file)
+
+    def get_page_title(self):
+        return self.config["DEFAULT"].get("PAGE_TITLE").split(", ")
+
+    def get_llm_options(self):
+        return self.config["DEFAULT"].get("LLM_OPTIONS").split(", ")
+    
+    def get_usecase_options(self):
+        return self.config["DEFAULT"].get("USECASE_OPTIONS").split(", ")
+        
+    def get_gemini_model_options(self):
+        return self.config["DEFAULT"].get("GEMINI_MODEL_OPTIONS").split(", ")
+    
+    
